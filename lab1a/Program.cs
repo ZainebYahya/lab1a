715afb975f009f0292ae08f735e0f5dec14936cb
ZainebYahya <znbyahya@gmail.com>
2018-01-23 14:55:52 -0500

    first commit

---
+++ lab1a/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                double width;
+                double length;
+                double hight;
+                double area;
+                double volume;
+                double perimeter;
+                string answer;
+
+            // input
+
+            do
+            {
+
+                Console.WriteLine("Welcome to Grand Circus'Room Detail Generator!");
+                Console.WriteLine();
+                Console.Write("Enter Length: ");
+                length = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter width: ");
+                width = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter hight: ");
+
+                hight = double.Parse(Console.ReadLine());
+
+
+                //processing
+
+                Console.WriteLine();
+                area = length * width;
+                perimeter = 2 * (length * width);
+                volume = length * width * hight;
+
+
+                //output
+                Console.WriteLine();
+                Console.WriteLine($"Area :{area}");
+                Console.WriteLine($"Perimeter {perimeter}");
+                Console.WriteLine($"Volume:{volume}");
+                Console.WriteLine();
+                Console.WriteLine("countinue? Y,N");
+                answer = Console.ReadLine();
+                if (answer == "n" || answer == "N")
+                {
+                    break;
+                }
+            }
+            while (answer == "Y" || answer == "y");
+
+            
+                
+            
+
+            
+                   
+            
+            
+                
+            
+            
+            
+            
+       
+
+            
+
+            }
+        }
+    }
+
+
+    
+
